d8644c5f4487f40606760cfd4f48968d681f90f2
Hare Krishna Kushwaha (Digital) <HA388684@wipro.com>
2019-11-08 10:25:22 +0530

    Express Web Application

---
+++ server.js
@@ -0,0 +1,18 @@
+
+const express = require('express');
+
+// Constants
+const PORT = 3000;
+const HOST = '0.0.0.0';
+
+// App
+const app = express();
+app.get('/', function(req, res) {
+  res.send('Hello this change is done!');
+});
+
+var port = process.env.PORT||PORT;
+app.listen(process.env.port || process.env.PORT || 3979, function () {
+  //console.log(`\n${ server.name } listening to ${ server.url } made some changes in the code`);
+  console.log('\nGet Bot Framework Emulator: https://aka.ms/botframework-emulator');
+});
